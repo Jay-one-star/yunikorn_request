bc89ba919ccab631f921bd47214e3d45b74a14f2
Jay-one-star <ama111138@gm.ntcu.edu.tw>
2025-11-26 01:49:11 +0800

    Update collect-cluster-stats.sh

---
+++ collect-cluster-stats.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+LOGFILE="cluster-stats-$TIMESTAMP.txt"
+
+{
+  echo "===== Cluster Stats @ $TIMESTAMP ====="
+  echo
+  echo "--- Node Usage ---"
+  kubectl describe nodes
+
+  echo "--- Pod Status ---"
+  kubectl get pods -A -o wide
+} > $LOGFILE
+
+echo "Saved: $LOGFILE"
+
+
+
+'''
+chmod +x collect-cluster-stats.sh
+
+
+crontab -e
+*/3 * * * *  /home/ama111138/yunikorn-test/collect-cluster-stats.sh
+
+'''
